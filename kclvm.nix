7dbc1bcd8d85449476cb08c5cb39aafe377cbab4
Oliver Berger <diefans@gmail.com>
2024-04-17 12:07:31 +0200

    fix: kclvm 0.8.1 compiles
    - we use oxalita overlay
    - the use of protobuf is enabled viw patch

---
+++ kclvm.nix
@@ -0,0 +1,97 @@
+{
+	rust_overlay ? import (fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz),
+	pkgs ? import (fetchTarball https://nixos.org/channels/nixos-23.11/nixexprs.tar.xz) { overlays = [ rust_overlay ]; },
+}:
+let
+    libPath = with pkgs; lib.makeLibraryPath [
+      # load external libraries that you need in your rust project here
+    ];
+	rustVersion = "latest";
+	# rustVersion = "1.76.0";
+	rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+		extensions = [
+			"rust-src" # for rust-analyzer
+				"rust-analyzer"
+		];
+	};
+
+in pkgs.rustPlatform.buildRustPackage rec {
+	pname = "kclvm";
+	version = "0.8.1";
+
+	src = pkgs.fetchFromGitHub {
+		owner = "kcl-lang";
+		repo = "kcl";
+		rev = "v${version}";
+		hash = "sha256-ayH7Mir6XREmxIjnfxUIe0mgFqww3N2Ge5kiYOMHuBU=";
+	}; # + /kclvm;
+	# https://discourse.nixos.org/t/difficulty-using-buildrustpackage-with-a-src-containing-multiple-cargo-workspaces/10202/5
+	sourceRoot = "source/kclvm";
+
+	# cargoSha256 = "sha256-FRD9Icrbcz+V8FCpc7B7BFEmkW+sIcXXK2hidp799yQ=";
+	cargoLock.lockFile = ./Cargo.lock.0.8.1;
+	cargoLock.outputHashes = {
+		"inkwell-0.2.0" = "sha256-JxSlhShb3JPhsXK8nGFi2uGPp8XqZUSiqniLBrhr+sM=";
+		#fakeSha256;
+	};
+
+	nativeBuildInputs = [
+		rust
+	];
+	buildInputs = [
+		# rust
+	  ] ++ (with pkgs; [
+      clang
+      # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
+      llvmPackages_12.bintools
+      # rustup
+	  # cargo
+	  # rustc
+	  glibc
+	  glibmm
+	  libxml2
+	  ncurses5
+	  protobuf
+	]);
+
+	patches = [./enable_protoc_env.patch];
+	# preBuild = ''
+	# '';
+
+	# RUST_BACKTRACE = "full";
+	# CARGO_PROFILE_RELEASE_BUILD_OVERRIDE_DEBUG = true;
+
+	LLVM_SYS_120_PREFIX = pkgs.llvmPackages_12.llvm.dev;
+	PROTOC = "${pkgs.protobuf}/bin/protoc";
+	PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+
+    LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_12.libclang.lib ];
+    # Add glibc, clang, glib, and other headers to bindgen search path
+    BINDGEN_EXTRA_CLANG_ARGS =
+    # Includes normal include path
+    (builtins.map (a: ''-I"${a}/include"'') [
+      # add dev libraries here (e.g. pkgs.libvmi.dev)
+	  # pkgs.glibc.dev
+    ])
+    # Includes with special directory paths
+    ++ [
+      ''-I"${pkgs.llvmPackages_12.libclang.lib}/lib/clang/${pkgs.llvmPackages_12.libclang.version}/include"''
+      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+    ];
+
+
+	# ERROR: The Cargo.lock contains git dependencies
+	# buildAndTestSubdir = "kclvm";
+	# sourceRoot = "kclvm";
+	# cargoLock = let
+	# 	fixupLockFile = path: f (builtins.readFile path);
+	# in {
+	# 	lockFileContents = fixupLockFile ./kclvm/Cargo.lock;
+	# };
+	# cargoLock = {
+	# 	lockFile = (./kclvm/Cargo.lock);
+	# };
+
+
+}
